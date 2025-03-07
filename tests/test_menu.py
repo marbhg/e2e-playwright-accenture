376e4d199668d200cb2c07886b73a97e34cd1cdb
Marrbe <132088541+marbhg@users.noreply.github.com>
2025-03-07 15:54:34 +0100

    act
    act

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -4,3 +4,15 @@ def test_visit_menu_links(page:Page):
   #Abrir la URL de la pagina
   print("Given the user visit home page Accenture")
   page.goto("https://www.accenture.com/es-es")
+
+  print("When the user aceppt the cookies")
+  #Localizamos el elemento por texto
+  locator = page.get_by_text("Aceptar todas las Cookies").click 
+
+  #Localizamos el elemento por el rol 
+  locator = page.get_by_role("link", name="Servicios").click()
+
+  
+
+
+
