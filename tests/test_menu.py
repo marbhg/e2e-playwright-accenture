0c86321c409dc6f07bd8d0e041ace6a309d94799
Marrbe <132088541+marbhg@users.noreply.github.com>
2025-03-12 20:05:30 +0100

    act
    act

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -7,10 +7,8 @@ def test_visit_menu_links(page:Page):
 
   print("When the user aceppt the cookies")
   #Localizamos el elemento por texto
-  locator = page.get_by_text("Aceptar todas las Cookies").click 
+  page.get_by_text("Aceptar todas las Cookies").click()
 
-   # Localizamos el elemento por el selector CSS y hacemos click
-  page.click("div.rad-button_text")
   
 
 
