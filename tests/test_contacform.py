31b7f439dc46c5425732e2b8cf0a1e5b589d8326
Marrbe <132088541+marbhg@users.noreply.github.com>
2025-03-14 20:55:38 +0100

    ACT
    ACT

---
+++ tests/test_contacform.py
@@ -0,0 +1,50 @@
+from playwright.sync_api import Page, expect
+ 
+#Arir la URL de la pagina
+def test_visit_contactfomr(page:Page):
+
+#Abrir la URL de la pagina 
+  print("Given the user visit home page Accenture")
+  page.goto("https://www.accenture.com/es-es/about/contact-us")
+
+  #Acepta las Cookies
+  print("When the user accept the cookies")
+  page.get_by_role("button", name="Aceptar todas las Cookies.").click()
+
+#El usuario Accede a la section de formulario 
+  print("When The user accesses the Jobseeker link")
+  page.get_by_role("button", name="Jobseeker ").click()
+
+  #El usuario rellena el formulario con el campo email vacio 
+  print("When the user fills in the name field")
+  page.get_by_role("textbox", name="* First Name").clear()
+  page.get_by_role("textbox", name="* First Name").fill("Maria")
+
+  print("When the user fills in the last name field")
+  page.get_by_role("textbox", name="* Last Name").clear()
+  page.get_by_role("textbox", name="* Last Name").fill("Lopez")
+
+  print("When the user selects the city Aruba")
+  page.get_by_role("combobox", name="* Country/Region required").click()
+  page.get_by_role("option", name="Aruba").click()
+
+  print("When the user fills out the section How can we help you?")
+  page.get_by_role("textbox", name="* How can we help you?").click()
+  page.get_by_role("textbox", name="* How can we help you?").fill("Quiero informacion acerca de sus tecnologias")
+
+  print("When the user accepts the privacy required")
+  page.get_by_role("group", name="* Privacy required").locator("span").nth(3).click()
+
+  print("When the user clicks the send button the from should display an error message")
+  page.get_by_role("button", name="ENVIAR").click()
+  expect(page.get_by_text("Email field is required and")).to_be_visible()
+
+
+
+
+ 
+
+ 
+
+
+
--- tests/test_menu.py
+++ tests/test_menu.py
@@ -9,6 +9,26 @@ def test_visit_menu_links(page:Page):
   #Localizamos el elemento por texto
   page.get_by_text("Aceptar todas las Cookies").click()
 
+  #Localizamos el elemento del enlace Servicio
+  print("When Visit the service link")
+  page.get_by_role("button", name="Servicios").click()
+
+  #Accedemos al elemento dentro del enlace Servicio
+  print("When Access the automation link")
+  page.get_by_role("menuitem", name="Automation").click()
+
+  #Acccedemos al enlace de quienes somos y entramos al apartado de Sostenibilidad
+  print("When Visit the Who We Are link and access the Sustainability section")
+  page.get_by_role("button", name="Quiénes Somos").click()
+  page.locator("#linklistteaser-a707da989f").get_by_role("menuitem", name="Sostenibilidad").click()
+  expect(page.get_by_role("link", name="Sostenibilidad", exact=True)).to_be_visible()
+
+  #Accedemos al apartado de incorporate y accedemos al enlace de oferta
+  print("When Visit the Join link and enter the Offer Finder link")
+  page.get_by_role("button", name="Incorpórate").click()
+  page.get_by_role("menuitem", name="Buscador de ofertas").click()
+
+
   
 
 
