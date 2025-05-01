55ecc50f9d898b50410b3927386c0fb93515f812
Marrbe <132088541+marbhg@users.noreply.github.com>
2025-05-01 20:03:53 +0200

    Act
    Act

--- tests/test_chage_language.py
+++ tests/test_chage_language.py
@@ -1,7 +1,8 @@
-
+import re
 from playwright.sync_api import Page, expect
 
-def test_changelanguage_chinesse(page:Page): 
+def test_change_language_chinese(page:page):
+
 
   #Abrir la URL de la pagina
   print("Given the user visit home page Accenture")
@@ -12,21 +13,17 @@ def test_changelanguage_chinesse(page:Page):
   
   print("When the user enters the language change link")
   page.get_by_role("button", name="Country and language selector").click()
-
   print("And the user changes the language to Chinese")
   page.get_by_role("menuitem", name="China/Mainland (Chinese)").click()
+  expect(page).to_have_url(re.compile(".*cn-zh*."))
 
-  print("When you enter the language link and change it to Chinese")
-  page.get_by_role("button", name="Country and language selector").click()
-  page.get_by_role("menuitem", name="China/Mainland (Chinese)").click()
-  print("When the user must view the page in Chinese")
-  page.get_by_role("button", name="Country and language selector").click()
     
-  print("When the user the language link and change it to Canada")
-  page.locator("li").filter(has_text="Canada (French)").click()
-  page.get_by_role("button", name="Country and language selector").click()
-  print("When the user must view the page in Canada")
-  page.get_by_role("menuitem", name="Canada (French)").click()
+def test_change_language_french(page:page):
+    print("When the user the language link and change it to Canada")
+    page.locator("li").filter(has_text="Canada (French)").click()
+    page.get_by_role("button", name="Country and language selector").click()
+    print("When the user must view the page in Canada")
+    page.get_by_role("menuitem", name="Canada (French)").click()
 
 
 
--- tests/test_homepage_menu.py
+++ tests/test_homepage_menu.py
@@ -7,27 +7,45 @@ def test_visit_menu_links(page:Page):
   print("Given the user visit home page Accenture")
   page.goto("https://www.accenture.com/es-es")
 
-  #Localizamos el elemento por texto
+ #Localizamos el elemento por texto
   print("When the user aceppt the cookies")
   page.get_by_role("button", name="Aceptar todas las Cookies.").click()
+  #Comprobamos el ancho de la pantalla
+  if(utils.is_mobile(page)):
+    print("When the user click on menu")
+    page.get_by_label("menu", exact=True).click()
+    page.get_by_role("button", name="Servicios", exact=True).click()
 
   print("When the user click on services")
-  page.get_by_role("button", name="Servicios").click()
+  page.get_by_role("button", name="Servicios").last.click()
   print("When the user click on automation, the expected page opens")
   page.get_by_role("link", name="Automation").click()
-  expect(page).to_have_url("automation")
+  expect(page).to_have_url(re.compile(".*automation.*"))
 
   page.goto("https://www.accenture.com/es-es")
   print("When the user click on Quienes somos")
-  page.get_by_role("button", name="Quiénes somos").click()
+  if(utils.is_mobile(page)):
+    print("When the user click on menu")
+    page.get_by_label("menu", exact=True).click()
+    page.get_by_role("button", name="Quiénes somos", exact=True).click()
+  page.get_by_role("button", name="Quiénes somos").last.click()
+
   print("When the user click on Sustainability, the expected page opens")
   page.locator("#globalnav-primarynavlinks-item1-5588bc7152").get_by_role("list").filter(has_text="Quiénes somos Equipo").get_by_label("Sostenibilidad").click()
   expect(page).to_have_url(re.compile(".*sustainability.*"))
 
+  page.goto("https://www.accenture.com/es-es")
   print("When the user click on the Offer Finder")
-  page.get_by_role("button", name="Incorpórate").click()
-  expect(page).to_have_url("Incorporate")
+  if(utils.is_mobile(page)):
+    print("When the user click on menu")
+    page.get_by_label("menu", exact=True).click()
+    page.get_by_role("button", name="Incorpórate").click()
+
+  if(utils.is_mobile(page)):
+    page.get_by_role("button", name="Únete a nuestro equipo").click()
+
   print("When the user enters the offer search link and directs me to the corresponding page")
   page.get_by_role("link", name="Buscador de ofertas").click()
+  expect(page).to_have_url(re.compile(".*careers.*"))
 
   
